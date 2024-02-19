4048285ba5cbe253a33c42afaeeadb95162a579b
chenxin9 陈鑫 <chenxin9@yiche.com>
2024-02-19 11:30:25 +0800

    respond to multiple PINGS

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,21 +6,21 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-  public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+    public static void main(String[] args) {
+        // You can use print statements as follows for debugging, they'll be visible when running tests.
+        System.out.println("Logs from your program will appear here!");
 
-    //  Uncomment this block to pass the first stage
+        //  Uncomment this block to pass the first stage
         ServerSocket serverSocket = null;
         Socket clientSocket = null;
-        String serverRepsonse = "+PONG\r\n";
+        String serverResponse = "+PONG\r\n";
         String clientMessage = null;
         int port = 6379;
         try {
-          serverSocket = new ServerSocket(port);
-          serverSocket.setReuseAddress(true);
-          // Wait for connection from client.
-          clientSocket = serverSocket.accept();
+            serverSocket = new ServerSocket(port);
+            serverSocket.setReuseAddress(true);
+            // Wait for connection from client.
+            clientSocket = serverSocket.accept();
 
             BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             PrintWriter outWriter = new PrintWriter(clientSocket.getOutputStream(), true);
@@ -28,20 +28,20 @@ public class Main {
             while ((clientMessage = input.readLine()) != null) {
                 System.out.println("Command received: " + clientMessage);
                 if (clientMessage.equalsIgnoreCase("ping")) {
-                    outWriter.print(serverRepsonse);
+                    outWriter.print(serverResponse);
                     outWriter.flush();
                 }
             }
         } catch (IOException e) {
-          System.out.println("IOException: " + e.getMessage());
-        } finally {
-          try {
-            if (clientSocket != null) {
-              clientSocket.close();
-            }
-          } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
-          }
+        } finally {
+            try {
+                if (clientSocket != null) {
+                    clientSocket.close();
+                }
+            } catch (IOException e) {
+                System.out.println("IOException: " + e.getMessage());
+            }
         }
-  }
+    }
 }
