1e32239f7e3fe4e262c6f6a41d2c770f1edf7715
chenxin9 陈鑫 <chenxin9@yiche.com>
2024-02-19 11:56:00 +0800

    handle concurrent clients

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,33 +4,29 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
+    private static final ExecutorService EXECUTOR_SERVICE =
+            Executors.newFixedThreadPool(5);
+
     public static void main(String[] args) {
         // You can use print statements as follows for debugging, they'll be visible when running tests.
         System.out.println("Logs from your program will appear here!");
 
+
         //  Uncomment this block to pass the first stage
         ServerSocket serverSocket = null;
         Socket clientSocket = null;
-        String serverResponse = "+PONG\r\n";
-        String clientMessage = null;
         int port = 6379;
         try {
             serverSocket = new ServerSocket(port);
             serverSocket.setReuseAddress(true);
             // Wait for connection from client.
-            clientSocket = serverSocket.accept();
-
-            BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            PrintWriter outWriter = new PrintWriter(clientSocket.getOutputStream(), true);
-
-            while ((clientMessage = input.readLine()) != null) {
-                System.out.println("Command received: " + clientMessage);
-                if (clientMessage.equalsIgnoreCase("ping")) {
-                    outWriter.print(serverResponse);
-                    outWriter.flush();
-                }
+            while (true) {
+                Socket accept = serverSocket.accept();
+                EXECUTOR_SERVICE.submit(new ClientHandler(accept));
             }
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
@@ -42,6 +38,45 @@ public class Main {
             } catch (IOException e) {
                 System.out.println("IOException: " + e.getMessage());
             }
+        }
+    }
+}
+
+class ClientHandler implements Runnable {
+
+    private final Socket clientSocket;
+
+    public ClientHandler(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+
+    /**
+     * When an object implementing interface <code>Runnable</code> is used
+     * to create a thread, starting the thread causes the object's
+     * <code>run</code> method to be called in that separately executing
+     * thread.
+     * <p>
+     * The general contract of the method <code>run</code> is that it may
+     * take any action whatsoever.
+     *
+     * @see Thread#run()
+     */
+    @Override
+    public void run() {
+        try (PrintWriter out =
+                     new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(
+                     new InputStreamReader(clientSocket.getInputStream()))) {
+            String input;
+            while ((input = in.readLine()) != null) {
+                System.out.println("Command received " + input);
+                if ("PING".equalsIgnoreCase(input)) {
+                    out.print("+PONG\r\n");
+                    out.flush();
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
