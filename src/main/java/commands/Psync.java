92a394718d189fcbf5d1e55733cef467c74a1fc3
chenxin9 陈鑫 <chenxin9@yiche.com>
2024-02-21 15:22:50 +0800

    single-replica propagation

--- src/main/java/commands/Psync.java
+++ src/main/java/commands/Psync.java
@@ -14,8 +14,9 @@ public class Psync implements Command {
 
     @Override
     public Reply execute(Request request) {
-        return Reply.multiNoStarReply(Reply.status(String.format("FULLRESYNC %s %s", RedisConfig.id, RedisConfig.offSet)));
-                                      // Reply.sink(Base64.getDecoder().decode(EMPTY_DB)));
+        return Reply.status(String.format("FULLRESYNC %s %s", RedisConfig.id, RedisConfig.offSet));
+        // return Reply.multiNoStarReply(Reply.status(String.format("FULLRESYNC %s %s", RedisConfig.id, RedisConfig.offSet)),
+        //                               Reply.sink(Base64.getDecoder().decode(EMPTY_DB)));
     }
 
     @Override
